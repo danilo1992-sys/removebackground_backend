21e24e902c7a520aeceb38c081bf8e23afaafe8e
danilo1992-sys <sanfracaceresdanilo@gmail.com>
2025-10-22 22:56:16 -0300

    Documentado la api

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import cors from 'cors'
 import express from 'express'
 import router from './routes/uploads.js'
 import download from './routes/download.js'
+import { apiReference } from '@scalar/express-api-reference'
+
 
 const app = express()
 
@@ -10,6 +12,15 @@ const port = 3000
 app.use(cors())
 app.use(router)
 app.use(download)
+
+app.use(
+  '/doc',
+  apiReference({
+    url: '/openapi.json',
+    theme: 'solarized',
+    content: documentation.getSpecAsJson()
+  }),
+)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
